e53c2eb76392f5e20e3e1f2b17cfff069bf38695
Thomas Apodaca <tma@angrymonkey.com>
2015-11-09 20:12:54 -0800

    strip extra dirs from the end of github paths

--- run_update.py
+++ run_update.py
@@ -374,6 +374,14 @@ def non_github_project_update_time(project):
     return project
 
 
+def make_root_github_project_path(path):
+    ''' Strip anything extra off the end of a github path
+    '''
+    path_split = path.split('/')
+    path = '/'.join(path_split[0:3])
+    return path
+
+
 def update_project_info(project):
     ''' Update info from Github, if it's missing.
 
@@ -398,6 +406,8 @@ def update_project_info(project):
     # Get the Github attributes
     if host == 'github.com':
         path = sub(r"[\s\/]+?$", "", path)
+        # make sure we're working with the main github URL
+        path = make_root_github_project_path(path)
         repo_url = GITHUB_REPOS_API_URL.format(repo_path=path)
 
         # If we've hit the GitHub rate limit, skip updating projects.
@@ -631,6 +641,8 @@ def get_issues_for_project(project):
     # Get github issues api url
     _, host, path, _, _, _ = urlparse(project.code_url)
     path = sub(r"[\s\/]+?$", "", path)
+    # make sure we're working with the main github URL
+    path = make_root_github_project_path(path)
     issues_url = GITHUB_ISSUES_API_URL.format(repo_path=path)
 
     # Ping github's api for project issues
@@ -681,6 +693,8 @@ def get_issues(org_name):
             continue
 
         path = sub(r"[\s\/]+?$", "", path)
+        # make sure we're working with the main github URL
+        path = make_root_github_project_path(path)
         issues_url = GITHUB_ISSUES_API_URL.format(repo_path=path)
 
         # Ping github's api for project issues
@@ -729,6 +743,8 @@ def get_root_directory_listing_for_project(project_dict, force=False):
     # Get the API URL
     _, host, path, _, _, _ = urlparse(project_dict['code_url'])
     path = sub(r"[\s\/]+?$", "", path)
+    # make sure we're working with the main github URL
+    path = make_root_github_project_path(path)
     directory_url = GITHUB_CONTENT_API_URL.format(repo_path=path, file_path='')
 
     # Request the directory listing
@@ -774,6 +790,8 @@ def get_civic_json_for_project(project_dict, force=False):
     # Get the API URL (if 'code_url' wasn't in project_dict, it would've been caught upstream)
     _, host, path, _, _, _ = urlparse(project_dict['code_url'])
     path = sub(r"[\s\/]+?$", "", path)
+    # make sure we're working with the main github URL
+    path = make_root_github_project_path(path)
     civic_url = GITHUB_CONTENT_API_URL.format(repo_path=path, file_path='civic.json')
 
     # Request the contents of the civic.json file
