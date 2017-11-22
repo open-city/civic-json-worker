ca0ece246c2883e1818e12448adf5b02f9c10974
Tom Dooner <tdooner@codeforamerica.org>
2017-11-22 11:59:49 -0800

    Support tag-based querying of organizations
    PR https://github.com/codeforamerica/brigade-information/pull/42 added
    "tags" information to the brigade-information repository. To help users
    of this data, like myself, filter the list effectively, this commit adds
    a tags query parameter to filter the list down.
    
    For simplicity, it uses PostgreSQL's JSONB data type to store and query
    the data.

--- models.py
+++ models.py
@@ -5,6 +5,7 @@ import json
 import time
 
 from flask import request
+from sqlalchemy.dialects.postgresql import JSONB
 from sqlalchemy.ext.mutable import Mutable
 from sqlalchemy.ext.compiler import compiles
 from sqlalchemy import types, desc
@@ -85,6 +86,7 @@ class Organization(db.Model):
     events_url = db.Column(db.Unicode())
     rss = db.Column(db.Unicode())
     projects_list_url = db.Column(db.Unicode())
+    tags = db.Column(JSONB())
     type = db.Column(db.Unicode())
     city = db.Column(db.Unicode())
     latitude = db.Column(db.Float())
@@ -100,12 +102,15 @@ class Organization(db.Model):
     # can contain events, stories, projects (these relationships are defined in the child objects)
 
     def __init__(self, name, website=None, events_url=None, members_count=None,
-                 rss=None, projects_list_url=None, type=None, city=None, latitude=None, longitude=None, last_updated=time.time()):
+                 rss=None, projects_list_url=None, type=None, city=None,
+                 latitude=None, longitude=None, last_updated=time.time(),
+                 tags=[]):
         self.name = name
         self.website = website
         self.events_url = events_url
         self.rss = rss
         self.projects_list_url = projects_list_url
+        self.tags = tags
         self.type = type
         self.city = city
         self.latitude = latitude
